bf2d60577c96aed660f3b228368dd333f970d0f0
gucaselo <gucaselo@gmail.com>
2021-05-08 23:19:26 -0400

    Final Code

--- test_flask/app_v1_working.py
+++ test_flask/app_v1_working.py
@@ -19,20 +19,11 @@ def index():
 
     return render_template("index.html")
 
-# create route that returns a geoJson dataset
-# @app.route("/geojson")
-# def geojson():
-
-#     return render_template("index.html")
-
 # create route that returns json data from database
 @app.route("/data")
 def data():
 
-    # Create Engine
-    # engine = create_engine(f"sqlite:///mass_shooting.sqlite")
-
-    engine = create_engine(f"sqlite:///data/msa.sqlite")
+    engine = create_engine(f"sqlite:///static/data/msa.sqlite")
 
     # Connect to the engine
     conn = engine.connect()
@@ -40,70 +31,95 @@ def data():
     # Start a session
     session = Session(bind=engine)
 
-    # Create variables for the dictionary
-    # lng = session.query(Shooting.longitude).all()
-    # lat = session.query(Shooting.latitude).all()
-    # killed = session.query(Shooting.number_killed).all()
-    # injured = session.query(Shooting.number_injured).all
-    # date = session.query(Shooting.incident_date).all()
-    # state = session.query(Shooting.state).all()
-    # locale = session.query(Shooting.city_county).all()
-
-    # data = pd.read_sql("SELECT * FROM Mass_Shooting_Data_USA", conn)
     data = pd.read_sql("SELECT * FROM msa", conn)
     data_json = data.to_json(orient='records', date_format='iso')
     msa_json = Response(response=data_json, status=200, mimetype='application/json')
-
-    # date = pd.read_sql("SELECT incident_date FROM Mass_Shooting_Data_USA", conn)
-    # state = pd.read_sql("SELECT state FROM Mass_Shooting_Data_USA", conn)
-    # locale = pd.read_sql("SELECT city_county FROM Mass_Shooting_Data_USA", conn)
-    # killed = pd.read_sql("SELECT number_killed FROM Mass_Shooting_Data_USA", conn)
-    # injured = pd.read_sql("SELECT number_injured FROM Mass_Shooting_Data_USA", conn)
-    # lat = pd.read_sql("SELECT latitude FROM Mass_Shooting_Data_USA", conn)
-    # lng = pd.read_sql("SELECT longitude FROM Mass_Shooting_Data_USA", conn)
-
-    # data = data.drop(['index', 'Unnamed: 0'], axis = 1)
-    
-    # data_json = data.to_json(orient='records')
-    # data_json = json.loads(data_json, mimetype='application/json')
-    # data_json = json.loads(data.to_json(orient='records'))
-    # data = jsonify(data)
-    # data_json = data.to_json(orient='records')
-    # data_json = (data.to_json(orient='records'), mimetype='application/json')
-    # data_json = (data_json, mimetype='application/json'))
-    # data_json =  make_response(json.dumps(data))
-    # data_json.headers['content-type'] = application/json
-    
-
-
-    # Create dictionary to be returned
-    # incident_list = {'date': date,
-    #             'state': state,
-    #             'city_county': locale,
-    #             'killed': killed,
-    #             'injured': injured,
-    #             'latitude': lat,
-    #             'longitude': lng}
-
-    # # Create dictionary to be returned
-    # incident_list = {'date': data['incident_date'].tolist(),
-    #     'state': data['state'].tolist(),
-    #     'city_county': data['city_county'].tolist(),
-    #     'killed': data['number_killed'].tolist(),
-    #     'injured': data['number_injured'].tolist(),
-    #     'latitude': data['latitude'].tolist(),
-    #     'longitude': data['longitude'].tolist()}
-
-
-    # data = jsonify(incident_list)
 
     # Close session
     session.close()
 
     return msa_json
-    # return response
-    # return data_json
-    # return jsonify(incident_list)
+
+@app.route("/guns")
+def guns():
+
+    engine = create_engine(f"sqlite:///static/data/msa.sqlite")
+
+    # Connect to the engine
+    conn = engine.connect()
+
+    # Start a session
+    session = Session(bind=engine)
+
+    data = pd.read_sql("SELECT * FROM ownership_data", conn)
+    data_json = data.to_json(orient='records', date_format='iso')
+    guns_json = Response(response=data_json, status=200, mimetype='application/json')
+
+    # Close session
+    session.close()
+
+    return guns_json
+
+@app.route("/poverty")
+def poverty():
+
+    engine = create_engine(f"sqlite:///static/data/msa.sqlite")
+
+    # Connect to the engine
+    conn = engine.connect()
+
+    # Start a session
+    session = Session(bind=engine)
+
+    data = pd.read_sql("SELECT * FROM poverty_data", conn)
+    data_json = data.to_json(orient='records', date_format='iso')
+    poverty_json = Response(response=data_json, status=200, mimetype='application/json')
+
+    # Close session
+    session.close()
+
+    return poverty_json
+
+@app.route("/election")
+def election():
+
+    engine = create_engine(f"sqlite:///static/data/msa.sqlite")
+
+    # Connect to the engine
+    conn = engine.connect()
+
+    # Start a session
+    session = Session(bind=engine)
+
+    data = pd.read_sql("SELECT * FROM election_data", conn)
+    data_json = data.to_json(orient='records', date_format='iso')
+    election_json = Response(response=data_json, status=200, mimetype='application/json')
+
+    # Close session
+    session.close()
+
+    return election_json
+
+@app.route("/state")
+def state():
+
+    engine = create_engine(f"sqlite:///static/data/msa.sqlite")
+
+    # Connect to the engine
+    conn = engine.connect()
+
+    # Start a session
+    session = Session(bind=engine)
+
+    data = pd.read_sql("SELECT * FROM state_data", conn)
+    data_json = data.to_json(orient='records', date_format='iso')
+    state_json = Response(response=data_json, status=200, mimetype='application/json')
+
+    # Close session
+    session.close()
+
+    return state_json
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test_flask/static/js/index.js
+++ test_flask/static/js/index.js
@@ -1,6 +1,8 @@
 // var link = "/static/data/us_counties.json";
 var link = "static/data/us_states.json";
 // var link = "static/data/us_congressional.json";
+var linkState = "static/data/state_clean.csv"
+var povertyLink = "static/data/poverty_data_cleaned.csv"
 
 // Function to generate random color codes //
 function randomColors(n) {
@@ -25,7 +27,6 @@ function getPoverty(name, data) {
   }
 }
 
-
 function getFillColor(name, data) {
   povertyData = getPoverty(name, data);
 
@@ -36,257 +37,201 @@ function getFillColor(name, data) {
                             '#FFC305';
   }
 
-      //----------------------------------------------//
-      //               Line Plot - Plotly             //
-      //----------------------------------------------//
-
-
-
-
-
-
-
-
-
 // MSA Json data
 d3.json("/data").then(function(data) {
-  // console.log(data[0].latitude[0])
-  console.log(data)
+
   // Grabbing our GeoJSON data..
   d3.json(link).then(function(geoData) {
-    // console.log(geoData.features[0].properties.NAME);
-    // console.log(geoData.features[0]);
+    console.log(geoData)
+
     // State coordinates
-    d3.csv("static/data/state_clean.csv").then(function(stateCoord) {
+    d3.csv(linkState).then(function(stateCoord) {
+    // d3.csv("/state").then(function(stateCoord) {
+      console.log(stateCoord)
 
       // Poverty data
-      d3.csv("static/data/poverty_data_cleaned.csv").then(function(povertyData) {
+      d3.csv(povertyLink).then(function(povertyData) {
 
         // Gun ownership dataset
-        d3.csv("static/data/gun_ownership_clean.csv").then(function(gunOwnership) {
+        // d3.csv("static/data/gun_ownership_clean.csv").then(function(gunOwnership) {
         var a = parseFloat(povertyData[0]['2019_poverty'])
         var b = parseFloat(povertyData[1]['2019_poverty'])
         console.log(a + b)
 
-      var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-      });
-    
-
-      var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "dark-v10",
-      accessToken: API_KEY
-      });
-    
-      var waterColor = L.tileLayer.provider('Stamen.Watercolor');
-    
-      var nasa = L.tileLayer.provider('NASAGIBS.ViirsEarthAtNight2012');
-    
-      // Only one base layer can be shown at a time
-      var baseMaps = {
-          Street: streetmap,
-          Dark: dark,
-          Water: waterColor,
-          Nasa: nasa
-      };
-
-      // console.log(data)
-      // console.log(data[0].incident_date.slice(0,4))
-      // Grab all unique states values to create a dropdown menu
-      var states = []
-      for (var i=0;i<data.length;i++){
-        var currentState = data[i].state
-        if (!states.includes(currentState)) {
-            // Add an all option to array
-            if (i === 0) {
-              states.push("-All-")
-            }
-            states.push(currentState)
-        }
-      };
-      // Sort States Array
-      var sortedStates = states.sort()
-
-      // Update HTML by adding States to drop down menu
-      sortedStates.forEach((value) => {
-        var option = d3.select('select').append('option');
-        option.attr('value', value);
-        option.text(value);
-      });
-      // console.log(sortedStates)
-
-      // -------------------------------------------------- //
-      //              Incident Cluster Markers              //
-      // -------------------------------------------------- //
-
-      // Create a new marker cluster group
-      var markers = L.markerClusterGroup();
-
-      // // Create layer support group 
-      // var mcgLayerSupportGroup = L.markerClusterGroup.layerSupport(options);
-
-      // var markers = L.markerClusterGroup({
-      //   spiderfyOnMaxZoom: true,
-      //   showCoverageOnHover: true,
-      //   zoomToBoundsOnClick: true
-      // });
-
-      var shootingsMarkers = []
-      for (var i = 0; i < data.length; i++) {
-        var lat = +data[i].latitude;
-        var lng = +data[i].longitude;
-        var coordinates = [lat, lng];
-        // console.log(coordinates)
-
-        // Add a new marker to the cluster group and bind a pop-up
-        // markers.addLayer(L.marker([lat, lng])
-        //   .bindPopup(data[i].address));
-        shootingsMarkers.push(L.marker([lat, lng])
-        .bindPopup(data[i].address).update());
-        // .addTo(myMap);
-
-      }
-      // Create layer groups:
-      var shootings = L.layerGroup(shootingsMarkers);
-
-      ///////////////////////////////////////
-      /////////////////Legend///////////////
-      var legend = L.control({position: 'bottomright'});
-      // Create legend
-      legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'info legend'),
-          grades = [0, 5, 10, 15, 20],
-          labels = [];
-
-      // Loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < grades.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
-      return div;
-  };
-
-      ////////////////////////////////////////
-
-
-
-      // Create a map object
-      var myMap = L.map("map", {
-        center: [37.09, -95.71],
-        // center: ["42.407211", "-71.382437"],
-        zoom: 4,
-        layers:[streetmap, shootings]
-      });
-
-      var overlayMaps = {
-        Shootings: shootings,
-      };
-
+        var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+        });
       
-      // Add our marker cluster layer to the map
-      // myMap.addLayer(markers);
 
-      // Creating a geoJSON layer with the retrieved data
-      var stateLayer = L.geoJson(geoData, {
-        // Passing in our style object
-        style: function (feature) {
-          return {
-          color:"white",
-          // fillColor: randomColors(feature.features),
-          fillColor:getFillColor(feature.properties.NAME, povertyData),
-          fillOpacity: 0.5,
-          weight: 1.5
+        var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+        });
+      
+        var waterColor = L.tileLayer.provider('Stamen.Watercolor');
+      
+        var nasa = L.tileLayer.provider('NASAGIBS.ViirsEarthAtNight2012');
+      
+        // Only one base layer can be shown at a time
+        var baseMaps = {
+            Street: streetmap,
+            Dark: dark,
+            Water: waterColor,
+            Nasa: nasa
         };
-      }
-      }).addTo(myMap);
-    
-      // Add legend to map
-      legend.addTo(myMap);
 
-      // Pass our map layers into our layer control
-      // Add the layer control to the map
-      var controller = L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+        // Grab all unique states values to create a dropdown menu
+        var states = []
+        for (var i=0;i<data.length;i++){
+          var currentState = data[i].state
+          if (!states.includes(currentState)) {
+              // Add an all option to array
+              if (i === 0) {
+                states.push("-All-")
+              }
+              states.push(currentState)
+          }
+        };
+        // Sort States Array
+        var sortedStates = states.sort()
 
-      // // //----------------------------------------------//
-      // // //               Line Plot - Plotly             //
-      // // //----------------------------------------------//
+        // Update HTML by adding States to drop down menu
+        sortedStates.forEach((value) => {
+          var option = d3.select('select').append('option');
+          option.attr('value', value);
+          option.text(value);
+        });
 
+        // -------------------------------------------------- //
+        //              Incident Cluster Markers              //
+        // -------------------------------------------------- //
 
+        // Create a new marker cluster group
+        // var markers = L.markerClusterGroup();
 
+        // // Create layer support group 
+        // var mcgLayerSupportGroup = L.markerClusterGroup.layerSupport(options);
 
-//////////////////////////////////////////////////////////////////////
-      // var tbody = d3.select('tbody');
-      // var location = "United States"
-      // var noCapita = 0;
-      // var gunRegistered = 0;     
-      // for (i=0; i < gunOwnership.length; i++){
-      //         noCapita =+ gunOwnership[i]['number_per_capita'];
-      //         gunRegistered =+ +gunOwnership[i]['number_registered']
-      //       }
-      //       console.log(noCapita)
-      //       console.log(gunOwnership)
-      // var gunData = [noCapita, gunOwnership];
+        // var markers = L.markerClusterGroup({
+        //   spiderfyOnMaxZoom: true,
+        //   showCoverageOnHover: true,
+        //   zoomToBoundsOnClick: true
+        // });
 
-      // gunData.forEach(value => {
-      //   var rows = tbody.append('tr')
-      //   Object.entries(value).forEach(([key, value]) => {
-      //       var data = rows.append('td');
-      //       data.text(value);
-      //   }) 
-      // });
+        var shootingsMarkers = []
+        for (var i = 0; i < data.length; i++) {
+          var lat = +data[i].latitude;
+          var lng = +data[i].longitude;
+          var coordinates = [lat, lng];
+          // console.log(coordinates)
 
+          // Add a new marker to the cluster group and bind a pop-up
+          // markers.addLayer(L.marker([lat, lng])
+          //   .bindPopup(data[i].address));
+          shootingsMarkers.push(L.marker([lat, lng])
+          .bindPopup(data[i].address).update());
+          // .addTo(myMap);
 
+        }
+        // Create layer groups:
+        var shootings = L.layerGroup(shootingsMarkers);
 
+        
+        //------------- Legend ---------------//
+        var legend = L.control({position: 'bottomright'});
+        // Create legend
+        legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 5, 10, 15, 20],
+            labels = [];
 
+        // Loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
 
+        // Create a map object
+        var myMap = L.map("map", {
+          center: [37.09, -95.71],
+          zoom: 4,
+          layers:[streetmap, shootings]
+        });
 
+        var overlayMaps = {
+          Shootings: shootings,
+        };
 
+        
+        // Add our marker cluster layer to the map
+        // myMap.addLayer(markers);
 
-
-
-
-////////////////////////////////////////////////////////////////////
-
-
-
-
-      //----------------------------------------------//
-      //               Summary Table                  //
-      //----------------------------------------------//
-      // var tbody = d3.select('tbody');
-      // var location = "United States"
-
-
+        // Creating a geoJSON layer with the retrieved data
+        var stateLayer = L.geoJson(geoData, {
+          // Passing in our style object
+          style: function (feature) {
+            return {
+            color:"white",
+            // fillColor: randomColors(feature.features),
+            fillColor:getFillColor(feature.properties.NAME, povertyData),
+            fillOpacity: 0.5,
+            weight: 1.5
+          };
+        }
+        }).addTo(myMap);
       
+        // Add legend to map
+        legend.addTo(myMap);
 
-      
-
-
-
-
-
-
+        // Pass our map layers into our layer control
+        // Add the layer control to the map
+        var controller = L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 
 
 
-      //----------------------------------------------//
-      //----------------------------------------------//
+        //----------------------------------------------//
+        //               Summary able                   //
+        //----------------------------------------------//
 
-      // Default State
-      // console.log(d3.select('#selDataset').property("value"))
 
-      // Event listeners
-      // d3.select('#selDataset').on("change", stateSelection);
-      d3.select('#selDataset').on("change", updateMap);
+        // var tbody = d3.select('tbody');
+        // var location = "United States"
+        // var noCapita = 0;
+        // var gunRegistered = 0;     
+        // for (i=0; i < gunOwnership.length; i++){
+        //         noCapita =+ gunOwnership[i]['number_per_capita'];
+        //         gunRegistered =+ +gunOwnership[i]['number_registered']
+        //       }
+        //       console.log(noCapita)
+        //       console.log(gunOwnership)
+        // var gunData = [noCapita, gunOwnership];
+
+        // gunData.forEach(value => {
+        //   var rows = tbody.append('tr')
+        //   Object.entries(value).forEach(([key, value]) => {
+        //       var data = rows.append('td');
+        //       data.text(value);
+        //   }) 
+        // });
+        /////////////////////////////////////////////////
+
+        // Default State
+        // console.log(d3.select('#selDataset').property("value"))
+
+        // Event listeners
+        // d3.select('#selDataset').on("change", stateSelection);
+        d3.select('#selDataset').on("change", updateMap);
 
 
 
@@ -301,18 +246,13 @@ d3.json("/data").then(function(data) {
             if (selection === stateCoord[i].state){
               var coordinate = [stateCoord[i].latitude, stateCoord[i].longitude];
               var zoom = 7;
-              // console.log(selection);
-              // console.log(coordinate);
-              // console.log(i)
               break
-              
             }
             else if (selection === '-All-') {
               var coordinate = defaultCoord;
               var zoom = 4;
               console.log('Entire country selected');
               console.log(defaultCoord);
-              // console.log(i)
               break
             }
           }
@@ -320,7 +260,6 @@ d3.json("/data").then(function(data) {
           myMap.flyTo(coordinate, zoom)
           // myMap.panTo(coordinate, zoom)
           // myMap.setView(coordinate, zoom)
-          // console.log('found value')
 
           myMap.removeControl(controller);
           myMap.removeLayer(shootings);
@@ -331,20 +270,14 @@ d3.json("/data").then(function(data) {
             lat = +data[i].latitude;
             lng = +data[i].longitude;
             if (selection === data[i].state) {
-              // console.log(data[i].state)
               shootingsMarkersUpdated.push(L.marker([lat, lng])
               .bindPopup(data[i].address));
-              // console.log(shootingsMarkers)
             }
             else if (selection === '-All-') {
               shootingsMarkersUpdated.push(L.marker([lat, lng])
               .bindPopup(data[i].address));
             }
           }
-          
-          // console.log(geoData.features[0].properties.NAME)
-          console.log(counties)
-          console.log(geoData)
           
           // Grab state ID to select counties when a state is selected by user
           for (i=0; i < geoData.features.length; i++) {
@@ -422,7 +355,7 @@ d3.json("/data").then(function(data) {
       }; // function
 
 
-      }); //Gun Ownership
+      // }); //Gun Ownership
       }); //Poverty Data
     }); //StateCoord
   });//geoJson
@@ -433,38 +366,3 @@ function stateSelection(states){
   console.log(d3.select(this).property("value"))
   // console.log(d3.select('#selDataset').property("value"))
 };
-
-//
-
-// function updateMap(data, geoData, stateCoord){
-//   console.log(d3.select('#selDataset').property("value"))
-  
-//   // var selection = d3.select('#selDataset').property("value")
-//   // console.log(selection)
-//   // var coordinates = stateSelection()
-//   // var names = states.map(function(data) {
-//   //   return data.name;
-//   // });
-//   // for (i=0; i < stateCoord.length; i++){
-//   //   if (selection === stateCoord[i].name){
-//   //     console.log(coordinate = [stateCoord[i].latitude, stateCoord[i].longitude]);
-//   //   }
-//   //   else if (selection === '-All-') {
-//   //     console.log('Entire country selected');
-//   //   }
-//   // }
-// };
-
-
-
-
-// // Event listeners
-// d3.select('#selDataset').on("change", stateSelection);
-// d3.select('#selDataset').on("change", updateMap);
-// d3.select(window).on('load', stateSelection);
-
-
-// d3.select('#selDataset').property("value").on('change', function(){
-//   var id = d3.select(this)
-//   console.log(id.property("value"))
-// });
